05b0f36d6ee058c6d13937adcb299a26380bf579
Mike Dillon <mike@embody.org>
2015-07-24 12:15:50 -0700

    Fix whitespace
    [skip ci]

--- update.sh
+++ update.sh
@@ -15,10 +15,10 @@ packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
 curl -sSL "${packagesUrl}.bz2" | bunzip2 > "$packages"
 
 for version in "${versions[@]}"; do
-  IFS=- read pg_major postgis_major <<< "$version"
+	IFS=- read pg_major postgis_major <<< "$version"
 	fullVersion="$(grep -m1 -A10 "^Package: postgresql-$pg_major-postgis-$postgis_major\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
 	(
-    set -x
+		set -x
 		cp Dockerfile.template initdb-postgis.sh README.md "$version/"
 		mv "$version/Dockerfile.template" "$version/Dockerfile"
 		sed -i 's/%%PG_MAJOR%%/'$pg_major'/g; s/%%POSTGIS_MAJOR%%/'$postgis_major'/g; s/%%POSTGIS_VERSION%%/'$fullVersion'/g' "$version/Dockerfile"
