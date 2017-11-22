7ff89fa1ba94ca14aa14a59a0cf1b43a25a36ded
Jacob Hayes <jacob.r.hayes@gmail.com>
2017-11-22 15:42:48 -0600

    Bump postgis version and improve update-postgis.sh

--- 9.4-2.4/alpine/update-postgis.sh
+++ 9.4-2.4/alpine/update-postgis.sh
@@ -8,9 +8,9 @@ export PGUSER="$POSTGRES_USER"
 POSTGIS_VERSION="${POSTGIS_VERSION%%+*}"
 
 # Load PostGIS into both template_database and $POSTGRES_DB
-for DB in template_postgis "$POSTGRES_DB"; do
+for DB in template_postgis "$POSTGRES_DB" "${@}"; do
     echo "Updating PostGIS extensions '$DB' to $POSTGIS_VERSION"
-    psql --dbname="$DB" -c "
+    psql -p "${POSTGRES_PORT:-5432}" --dbname="$DB" -c "
         -- Upgrade PostGIS (includes raster)
         CREATE EXTENSION IF NOT EXISTS postgis VERSION '$POSTGIS_VERSION';
         ALTER EXTENSION postgis  UPDATE TO '$POSTGIS_VERSION';
