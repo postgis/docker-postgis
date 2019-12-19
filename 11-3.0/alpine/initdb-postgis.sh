7451856c98cdb343330f8abf9e4c6eb94e1252d0
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2019-12-19 16:32:02 +0700

    use IS_TEMPLATE param for template_postgis creation for PG > 9.4

--- 11-3.0/alpine/initdb-postgis.sh
+++ 11-3.0/alpine/initdb-postgis.sh
@@ -7,8 +7,7 @@ export PGUSER="$POSTGRES_USER"
 
 # Create the 'template_postgis' template db
 "${psql[@]}" <<- 'EOSQL'
-CREATE DATABASE template_postgis;
-UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
+CREATE DATABASE template_postgis IS_TEMPLATE true;
 EOSQL
 
 # Load PostGIS into both template_database and $POSTGRES_DB
