27f716f9b7212870c497d6d6caf68f7a44c26bc0
rahul-ve <rv.work@outlook.com>
2021-02-09 02:08:25 +1100

    Add pcre library to templates, pcre is a dependency for address_standardizer extension (#220)
    * added pcre to templates, needed for address_standardizer extension
    
    * removed a typo
    
    * updated all Dockerfiles by running make
    
    * updated master template with proj temp fix and generated the dockerfiles
    
    * fixed goes commit as temp fix, added test address_standardizer test case
    
    * added libpcre3/pcre to final build
    
    * forgot to run make update before commiting last time

--- test/tests/postgis-basics/run.sh
+++ test/tests/postgis-basics/run.sh
@@ -37,3 +37,15 @@ done
 
 echo 'SELECT PostGIS_Version()' | psql
 [ "$(echo 'SELECT ST_X(ST_Point(0,0))' | psql)" = 0 ]
+
+
+## test address_standardizer extension
+echo 'CREATE EXTENSION address_standardizer;' | psql
+response=$(echo $'SELECT zip FROM parse_address(\'1 Devonshire Place, Boston, MA 02109-1234\') AS a;' | psql)
+if [ $response  = 02109 ]; then 
+	echo "address_standardizer extension installed and works!"
+else 
+	echo "address_standardizer extension test failed, returned response is $response"
+	exit 1
+fi
+
