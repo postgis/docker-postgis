2aa34e087141a80bd81e481f34302d1fafb5e2ac
ImreSamu <ImreSamu@users.noreply.github.com>
2024-03-23 19:42:35 +0100

    add auto update v2.

--- tools/get_versions_changes.sh
+++ tools/get_versions_changes.sh
@@ -6,15 +6,15 @@ set -Eeuo pipefail
 
 # example outputs: DUCKDB:v0.10.1,MOBILITYDB:v1.1.0
 
-git diff versions.json | \
-    grep "^+" | \
-    sed 's/^+//' | \
-    grep -E "_CHECKOUT|_VERSION" | \
-    grep -vE "_SHA1" | \
-    sed 's/_CHECKOUT"//g;s/_VERSION"//g' | \
-    sed 's# "tags/##g' | \
-    sed 's#"##g' | \
-    sed 's# ##g' | \
-    sed 's#,##g' | \
-    sort | uniq  | \
+git diff versions.json |
+    grep "^+" |
+    sed 's/^+//' |
+    grep -E "_CHECKOUT|_VERSION" |
+    grep -vE "_SHA1" |
+    sed 's/_CHECKOUT"//g;s/_VERSION"//g' |
+    sed 's# "tags/##g' |
+    sed 's#"##g' |
+    sed 's# ##g' |
+    sed 's#,##g' |
+    sort | uniq |
     awk 'BEGIN { ORS=""; print ""; total_length=0; } { word_length=length($0)+1; if (total_length+word_length<=80) { print (NR>1?",":"") $0; total_length+=word_length; } else { exit; } }'
